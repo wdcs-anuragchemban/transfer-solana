21ee09d7d4192783fcf97ba7b5bf56d0ee5d2d2f
codezeros <anurag.chemban@codezeros.com>
2023-05-04 12:22:46 +0530

    transfer solana

--- tests/transfer-sol.ts
+++ tests/transfer-sol.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { TransferSol } from "../target/types/transfer_sol";
+import { assert } from "chai";
 
 describe("transfer-sol", () => {
   // Configure the client to use the local cluster.
@@ -10,14 +11,15 @@ describe("transfer-sol", () => {
   const program = anchor.workspace.TransferSol as Program<TransferSol>;
   const systemAccount = anchor.web3.SystemProgram.programId;
 
-
   it("Transfer Sol", async () => {
     const toAddress = new anchor.web3.PublicKey(
       "FSVp8VAgP9pfFie1CpwvH5ek9GHkENbS9SavGSuyTVbU"
     );
 
+    const balance = await provider.connection.getBalance(toAddress);
+
     const tx = await program.methods
-      .transferSol(new anchor.BN(3000000000))
+      .transferSol(new anchor.BN(1000000))
       .accounts({
         from: provider.publicKey,
         to: toAddress,
@@ -26,5 +28,9 @@ describe("transfer-sol", () => {
       .rpc();
 
     console.log(tx);
+
+    const newBalance = await provider.connection.getBalance(toAddress);
+
+    assert.strictEqual(1000000, newBalance - balance);
   });
 });
